22efa9bbe001df33c4ef09bd9ff5ca1e27310957
xol48 <94368164+xol48@users.noreply.github.com>
2021-12-24 20:14:56 +0700

    f

---
+++ WpfApp2/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+namespace WpfApp2
+{
+    class Class1 : UserControl
+    {
+        public event 
+
+        /// <summary>
+        /// notify parent class
+        /// </summary>
+        /// <param name="arguments"></param>
+        protected void RaiseEventHandler(List<string> arguments)
+        {
+            NewArgumentEvent?.Invoke(arguments);
+        }
+
+        /// <summary>
+        /// clear all arguments
+        /// </summary>
+        virtual public void Clear() { }
+    }
+}
---
+++ WpfApp2/LowerCase_RemoveSpaces.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for LowerCase_RemoveSpaces.xaml
+    /// </summary>
+    public partial class LowerCase_RemoveSpaces : Window
+    {
+        public LowerCase_RemoveSpaces()
+        {
+            InitializeComponent();
+        }
+
+        private void choiceButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
